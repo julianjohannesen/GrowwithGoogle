78c94e71de4ff8003b8622006cd6f514fdedac51
Julian Johannesen <julian.johannesen@gmail.com>
2018-05-20 16:16:51 -0400

    A lot of work on for... of loops and a bit on destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,7 +1,19 @@
-// Loops and iteration
+// Destructuring arrays and objects
 
-myArray = ["thing1", "thing2", "thing3"]
+// ES6 gives us ashorthand to create variables corresponding to the elements or values of any array or object.
+
+const myArray = ["thing1", "thing2", "thing3"]
 
 const [var1, var2, var3] = myArray
 
 console.log(var1 + var2 + var3);
+
+const myObject = {
+    name1: "thing1",
+    name2: "thing2",
+    name3: "thing3"
+}
+
+const {name1, name2, name3} = myObject;
+
+console.log(name1 + name2 + name3);
--- looping-and-iterating.js
+++ looping-and-iterating.js
@@ -67,46 +67,72 @@ function continueStatement() {
 // FOR ... OF
 /////////////
 
-//The for...of iteration construct allows us to iterate over the members of an array
+//The for...of iteration construct allows us to iterate over any iterable object, e.g. arrays and strings. Ordinary objects are not iterable.
+
+//In for...of loops, abrupt iteration termination can be caused by break, continue[4], throw or return[5]. In these cases, the iterator is closed.
+
 const myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
- 
-function iterateOverValues(array){
-    for (const element of array) {
-    console.log(element);
-    }
-}
-
-const myString = 'Hello, world!';
-
-function iterateOverValues2(myString){
-    for (const char of myString) {
-        console.log(char);
-    }
-}
-
-//////////////
-// FOR ... IN
-//////////////
-
-//The for...in iteration construct allows us to iterate over the keys of an object
-  const address = {
+const myString = "Hello, World!"
+const address = {
     street1: '11 Broadway',
     street2: '2nd Floor',
     city: 'New York',
     state: 'NY',
     zipCode: 10004
-  };
+};
+
+function iterateOverValues(iterableThing){
+    for (const element of iterableThing) {
+    console.log(element);
+    }
+}
+
+iterateOverKeys(address);
+
+//Another example, using an array and demostrating that you can exit out of a for of loop using "continue"
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function iterateOverValues2(anArray) {
+    for (const value of anArray) {
+        if (value % 2 === 0) {
+            continue;
+        }
+        console.log(value); // => Will log the odd numbers
+    }
+}
+
+iterateOverValues(myArray);
+iterateOverValues(myString);
+//iterateOverValues(address); // => Uncaught TypeError
+iterateOverValues2(digits);
+
+//Return an array with the first letter of each day capitalized.
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+// your code goes here
+for (const day of days){
+    // Turn each day into an array
+    let dayUppercase =  day.split("");
+    // Grab the first letter of each day and make it uppercase
+    const firstLetter = dayUppercase.shift().toUpperCase();
+    // Insert the first letter into the array at index 0
+    dayUppercase.unshift(firstLetter);
+    // Join the elements of the day array back into a string
+    dayUppercase = dayUppercase.join('');
+    console.log(dayUppercase);
+}
+
+
+//////////////
+// FOR ... IN
+//////////////
+
+//The for...in iteration construct allows us to iterate over the keys of an object. It will iterate over all "enumerable properties." That means that if you add a method to an object's prototype, it will iterate over that method as well.
    
 function iterateOverKeys(object){
   for (const key in object) {
-      console.log(key);
+      console.log(`The key is '${key}' and the value is '${object[key]}.'`);
   }
-}
-
-function iterateOverKeys2(object){
-    for(const key in object){
-        console.log(object[key]);
-    }
 }
 
 
